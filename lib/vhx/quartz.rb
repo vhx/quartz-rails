db36178a7a8b6621252ee44031a94960ffd709ce
Steven Bone <steven@stevenbone.com>
2015-10-19 11:57:18 -0400

    Simplify gem requirements

--- lib/vhx/quartz.rb
+++ lib/vhx/quartz.rb
@@ -1,14 +1,7 @@
 require "vhx/quartz/version"
 
-if !defined?(Rails)
-  require 'Rails'
-end
-
 module Vhx
   module Quartz
-    module Rails
-      class Engine < ::Rails::Engine
-      end
-    end
+    class Engine < ::Rails::Engine
   end
 end
