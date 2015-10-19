c27e09329657fd19d4fbfa0d2dd367cb5ebe4322
Steven Bone <steven@stevenbone.com>
2015-10-19 12:05:34 -0400

    bump version to 0.2.12

--- lib/vhx/quartz.rb
+++ lib/vhx/quartz.rb
@@ -3,5 +3,6 @@ require "vhx/quartz/version"
 module Vhx
   module Quartz
     class Engine < ::Rails::Engine
+    end
   end
 end
--- lib/vhx/quartz/version.rb
+++ lib/vhx/quartz/version.rb
@@ -1,5 +1,5 @@
 module Vhx
   module Quartz
-    VERSION = "0.2.1101"
+    VERSION = "0.2.12101"
   end
 end
